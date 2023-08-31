cd4d3742073e92fc9f8db2430e30ecac519bfde4
yassir benjilali <benjilaliyassir@forleaders.net>
2023-08-31 17:51:45 +0100

    draw package masked

--- amazigh/interface/main.py
+++ amazigh/interface/main.py
@@ -32,13 +32,13 @@ def trim(image):
     return image
 
 # Specify canvas parameters in application
-drawing_mode = st.sidebar.selectbox(
+#drawing_mode = st.sidebar.selectbox(
     "Drawing tool:", ("point", "freedraw")
 )
 
-stroke_width = st.sidebar.slider("Stroke width: ", 1, 25, 3)
-if drawing_mode == 'point':
-    point_display_radius = st.sidebar.slider("Point display radius: ", 1, 25, 3)
+#stroke_width = st.sidebar.slider("Stroke width: ", 1, 25, 3)
+#if drawing_mode == 'point':
+#    point_display_radius = st.sidebar.slider("Point display radius: ", 1, 25, 3)
 #stroke_color = st.sidebar.color_picker("Stroke color hex: ")
 #bg_color = st.sidebar.color_picker("Background color hex: ", "#eee")
 #bg_image = st.sidebar.file_uploader("Background image:", type=["png", "jpg"])
