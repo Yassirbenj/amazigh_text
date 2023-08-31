5aa968a5585a025eedb28cbb8a8edb1eb4400dea
yassir benjilali <benjilaliyassir@forleaders.net>
2023-08-31 17:45:09 +0100

    draw package added

--- amazigh/interface/main.py
+++ amazigh/interface/main.py
@@ -2,6 +2,7 @@ import streamlit as st
 import tensorflow as tf
 import numpy as np
 from PIL import Image,ImageChops
+from streamlit_drawable_canvas import st_canvas
 
 
 def load_model():
@@ -29,9 +30,37 @@ def trim(image):
         image=image.crop(box)
     return image
 
+# Specify canvas parameters in application
+drawing_mode = st.sidebar.selectbox(
+    "Drawing tool:", ("point", "freedraw")
+)
+
+stroke_width = st.sidebar.slider("Stroke width: ", 1, 25, 3)
+if drawing_mode == 'point':
+    point_display_radius = st.sidebar.slider("Point display radius: ", 1, 25, 3)
+#stroke_color = st.sidebar.color_picker("Stroke color hex: ")
+#bg_color = st.sidebar.color_picker("Background color hex: ", "#eee")
+#bg_image = st.sidebar.file_uploader("Background image:", type=["png", "jpg"])
+
+realtime_update = st.sidebar.checkbox("Update in realtime", True)
+
 with st.form("input_form",clear_on_submit=True):
     st.write("<h3>Upload your image for the magic ✨</h3>", unsafe_allow_html=True)
-    input_img = st.file_uploader('character image',type=['png', 'jpg','jpeg'])
+    canvas_result = st_canvas(
+                    fill_color="rgba(255, 165, 0, 0.3)",  # Fixed fill color with some opacity
+                    stroke_width=stroke_width,
+                    #stroke_color=stroke_color,
+                    #background_color=bg_color,
+                    #background_image=Image.open(bg_image) if bg_image else None,
+                    update_streamlit=realtime_update,
+                    height=150,
+                    drawing_mode=drawing_mode,
+                    point_display_radius=point_display_radius if drawing_mode == 'point' else 0,
+                    key="canvas",
+                    )
+    input_img=canvas_result.image_data
+
+    #input_img = st.file_uploader('character image',type=['png', 'jpg','jpeg'])
     if st.form_submit_button("Predict"):
         if input_img:
             image = Image.open(input_img)
